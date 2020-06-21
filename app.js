8131445dd46d4f78e19f2801e45d64b691903247
Arham-khan641 <Arhamkhanhere@gmail.com>
2020-06-21 17:22:29 +0500

    javascript assignment-6

---
+++ app.js
@@ -0,0 +1,604 @@
+    // Arham Khan Roll no: WM12788
+    //Email: Arhamkhanhere@gmail.com
+
+
+
+// chapter   No21
+//-----------------------------------------------
+//question no 1
+
+
+          // var first;
+          // var last;
+          // var fullName;
+        
+          // first=prompt(" input First-Name","");
+          // last=prompt("input last-Name","");
+          // fullName=first+last;
+        
+          // window.alert('hello \n'+""  + fullName );
+
+
+//------------------------------------------------------------------------
+// qestion No 2
+
+
+    // // var getmodel;
+    // //  var  str;
+    // //   getmodel=prompt("input your favourit mobile phone  model","Samsung  Galaxy S6 Edge plus") ;
+    // //   var  str=getmodel.length;
+    // //   document.write("my favourit phone is:"+   getmodel+ " ",  "length of string is:",+  str);
+
+
+
+
+//--------------------------------------------------------------------------
+     // question No3 
+
+
+    //       // var str ="pakistani";
+          
+    //       //   var n = str.indexOf("n");
+
+    //       //   alert('String: Pakistani\n  Index of n:'+" "+ n);
+
+    //-------------------------------------------------------------------        
+    //  // question No 4
+    //         //  var str ="Hello world";
+                  
+    //         //  var l = str.lastIndexOf("l");
+
+    //         //  document.write("String:" +'Hello World\n', "Last Index of l:", + l);      
+            
+    //----------------------------------------------------------------------------         
+
+    //         //question 5
+
+
+    //         // var str ="pakistani";
+          
+    //         // var n = str.indexOf("i");
+
+    //         // alert('String: Pakistani\n Character at index'+" " + n +':'+" i");
+
+    //-----------------------------------------------------------------------
+    //        //question 6
+
+          // var first;
+          // var last;
+          // var fullName;
+          // var greet;
+          // first=prompt(" input First-Name","");
+          // last=prompt("input last-Name","");
+          
+          // fullName= first.concat(last);
+          // window.alert('hello \n'+""  + fullName );
+//-------------------------------------------------------------------------
+      //question no7
+         
+//       var str="hyderabad";
+      
+// var res=str.replace("hyder","Islam");
+//   window.alert('City: Hyderabad \n'+" After replacement:"+" "+ res);
+        //---------------------------------------------------------
+            //question no 8
+        // var str="Ali &  Sami are best friends. They play cricket & football together.";
+
+        //   var message = str.replace(/&/g ,"and");
+        //   document.write(message);
+
+        //-----------------------------------------------------------------
+            //question  no9
+            
+            //   var str="472";
+            
+            //   var num=parseInt(str);
+            //   if (str="472"){
+            //       alert('Value:'+ 472+'\n'+'type:string\n'+ 'Value:'+num +'\n'+''+'type:number');
+
+            //   }
+            //---------------------------------------------
+            //question no10
+            // var input;
+            // var conv;
+            // input=prompt("input a word","peanuts");
+            // conv=input.toUpperCase();
+            // window.alert('User input:'+input+'\n'+'Upper Case:'+conv );
+  
+            
+            //--------------------------------------------------------------
+            //question no11
+            
+            // var input;
+            // var conv;
+            // input=prompt("input a word","javascript");
+
+
+            // var conv=input[0].toUpperCase() 
+            // +input.slice(1); 
+                 
+                
+            // //   =input.toUpperCaseeach(0);        
+                                     
+            // window.alert('User input:'+ input+'\n'+'Upper Case:'+ conv );
+          
+            //---------------------------------------------------------------
+            // //question No 12
+            // var num=35.36;
+            // var str=num.toFixed(2);
+            // alert(str);
+        
+       //----------------------------------------------------------------
+       
+       // Question No 13
+
+//     alert('Bakery Items :\n'  +'cake,' +' apple pie,'+'  cookie,'+' chips,'+' patties');
+
+//        var name = prompt("Welcome  to ABC Bakery. What do you  Want to order"+'\n'+"sir/ma'am","cookie");
+//        var itmes=name;
+//        var quantity = prompt("Quantity ordered is","1");
+//        if (itmes=="cake")  {
+//            alert(itmes+'  is available  at index 2 in our bakery');
+//          }
+//          else if(itmes=="apple pie"){
+//             alert(itmes+'is available at index 2 in our bakery');
+
+//          }
+//          else if(itmes=="cookie"){
+//             alert(itmes+'is available at index 2 in our bakery');
+//          }
+//          else if(itmes=="chips"){
+//             alert(itmes+'is available at index 2 in our bakery');
+//          }
+//          else if(itmes=="patties"){
+//             alert(itmes+'is available at index 2 in our bakery');
+//          }
+         
+//          else {
+//         alert('We are sorry '  +  itmes + " is not available in our bakery"); 
+
+// }
+       
+//------------------------------------------------------
+    //question No 15
+
+    // var myInput;
+    // var letter ;
+    // var capital;
+    // var number ;
+    // var length ;
+    // var ps=prompt('The Password must qualify the requrements'+'\n'+ 'a.It should contain alphabets and numbers'+'\n'+'b. It should not start with a number'+'\n'+ 'c. It must at least 6 characters long ')
+
+    // if(myInput=ps.match(/[a-z]/g)){
+    //      var let=myInput;
+
+    // }
+    // else if(number=ps.match( /[09]/g)){
+    //        var num=number;
+      
+    // }
+    // else if (lenght=ps.value.length  <=6)
+    // {
+    //       var len=length;
+    // }
+    // else{
+    //   alert("invalid:password");
+    // }
+    //  document.write(let,num,len);
+//------------------------------------------
+    //question  No16
+
+    //   var university="university of karachi";
+    //    var str=university.split("");
+
+    //   document.write(str);
+
+//----------------------------------------------------
+
+        //questionNo17
+      //   var input;
+      //   var lst;
+      //   input=prompt("input user ","pakistan");
+      //   lst=input.charAt(input.length-1);
+      //    window.alert('User input :'+input+ '\n Last character  of input: '+ lst);
+//--------------------------------------------------------------------------------
+
+        // //questionNo 18
+            // var a="The quick brown fox"+" jumps over"+ "the lazy dog";
+            // var b=a.wordcount("The");
+            // document.write(b);
+
+
+
+
+
+
+        //  var str='The quick fox jump over the lazy dog;';
+        //  var chars=str.split("");
+        //  var chars=chars="the";
+        //  var word_count={};
+
+        //  for (var i=0; i < chars.length ;  i++)
+        //  {
+
+        //     if(word_count[chars[i]]==undefined);
+        //     word_count[chars[i]]=0
+        //     word_count[chars[i]]++;
+        //  }
+        //  for(var i in word_count)
+        //  {
+
+    //     document.write(i+'='+ word_count[i]);
+    //  }
+//----------------------------------------------------------------------
+// chapter no 26 to 30
+
+
+// ----------------------------------/
+  //question no 1
+
+
+      // var x,text;
+    
+
+
+    //   x=prompt("numb");
+      
+    //  // If x is Not a Number or less than one or greater than 10
+
+    //  if (isNaN(x) || x < 0 || x=="a-z" ) 
+        
+    //  {
+    //     text = "please inter positive integer";
+    //     alert(text);
+    //   }  
+              
+    //   else {
+
+    //          var w=Math.round(x);
+    //          var y=Math.floor(x);
+    //          var z=Math.ceil(x);
+    //         alert('a.number: '+ x+'\nb. round off value: '+w+ '\nc.floor  value: '+y+'\nd.ceil  value: '+z );
+        
+    //     }
+
+
+//---------------------------------------------------------------      
+  //quesion no  2
+
+
+  //    var a,b;
+  
+
+
+  //   a=prompt("numb");
+    
+  //  // If x is Not a Number is not less than zero
+
+  //  if (isNaN(a) || a > 0 || a=="a-z" ) 
+      
+  //  {
+  //     b = "please inter negative integer";
+  //     alert(b);
+  //   }  
+            
+  //   else {
+
+  //          var c=Math.round(a);
+  //          var d=Math.floor(a);
+  //          var e=Math.ceil(a);
+  //         alert('a.number: '+ a+'\nb. round off value: '+c+ '\nc.floor  value: '+d+'\nd.ceil  value: '+e );
+      
+  //     }
+ //-------------------------------------------------------
+
+    //question No 3
+
+    // var a;
+
+    //     a=prompt("user input");
+     
+    //    var b =Math.abs(a);
+
+  
+    //   window.alert(b);
+
+    //------------------------------------------
+    //question no4
+
+    
+
+      //  var a = Math.random(); 
+      //  var b = (a * 4) + 1; 
+      //   var c = Math.floor(b);
+ 
+      //   var g=a+b+c;
+      //   var f=Math.round(g)
+      //   alert('random dice value :'+f);
+
+//--------------------------------------------------------
+
+    //question no 5
+    
+    
+  //      var a = Math.random();
+  //     var b = (a * 1) ;
+
+  //     //  var c = Math.floor(b);
+
+  //      var g=a+b;
+  //      var f=Math.round(g)
+  //      if(f>=2){
+  //       alert(f +'\n  random coin value :'+'Head' );
+  //      }
+  //      else{
+  //        alert(f+'\n random coin value:'+' Tail')
+  //      }
+      
+
+//-------------------------------------------------------------
+
+  //     //question no 6
+
+  //  var a = Math.floor(Math.random() * 100) + 1;
+
+  // window.alert('random between 1 and 100: '+a)
+
+
+  //------------------------------------------------------------
+         //question no 7
+
+    // var a,b,c,d;
+
+    //   a=prompt("Enter you weights in kilograms")
+    //   if(a<9) {
+    //     alert(a+'kilogram')
+        
+    //   }
+    //   else if ( a>=9)
+    //   {
+    //       alert(a+"kilograms")
+          
+    //     }
+
+
+
+    //   else if (  a<=9,"kg", " kilo","kilograms")
+    //   {
+                
+    //     b=a.replace("kg","kilo");
+    //     c=b.replace("kilo","kilograms");
+    //     d=c.replace("kilos","kilogram");
+    //     e=d.replace("kilograms","kilogram");
+    //     f=e.replace("kgs","kilograms")
+    //     alert(e)
+    //   }
+
+    //------------------------------------------------------------------  
+     //question no 8
+    //  var a,b;
+     
+    //     a=Math.round(Math.floor(Math.random()*10))  +1;
+    //     b=prompt("Enter a number between 1 to 10");
+        
+    //     if(b==a){
+    //         window.alert("Congratulation" );
+    //     }
+    //     else{
+    //       window.alert("Try again!")
+
+    //     }
+//----------------------------------------------------------
+    //chapter 31 to 34
+  //-------------------------------------------------
+    //question no 1
+    
+      //   var a,b;
+      // a=new Date();
+      // document.write(a); 
+  //--------------------------------------------------------   
+      //question no 2
+      // var month = new Array();
+      // month[0] = "January";
+      // month[1] = "February";
+      // month[2] = "March";
+      // month[3] = "April";
+      // month[4] = "May";
+      // month[5] = "June";
+      // month[6] = "July";
+      // month[7] = "August";
+      // month[8] = "September";
+      // month[9] = "October";
+      // month[10] = "November";
+      // month[11] = "December";
+    
+      // var d = new Date();
+      // var n = month[d.getMonth()];
+      // window.alert('Current month: '+n);
+//----------------------------------------------------------------
+      //question  no 3
+      // var d = new Date();
+      // var weekday = new Array(7);
+      // weekday[0] = "Sun";
+      // weekday[1] = "Mon";
+      // weekday[2] = "Tue";
+      // weekday[3] = "Wed";
+      // weekday[4] = "Thu";
+      // weekday[5] = "Fri";
+      // weekday[6] = "Sat";
+
+      // var n = weekday[d.getDay()];
+
+      // document.write( 'Today is '+n); 
+//----------------------------------------------------
+        //question no 4
+
+        // var d = new Date();
+        // var weekday = new Array(7);
+        // weekday[0] = "Sun";
+        // weekday[1] = "Mon";
+        // weekday[2] = "Tue";
+        // weekday[3] = "Wed";
+        // weekday[4] = "Thu";
+        // weekday[5] = "Fri";
+        // weekday[6] = "Sat";
+
+        // var n = weekday[d.getDay()];
+        // if (n=="Sat","Sun"){
+        //    window.alert("It's Fun day")
+        // }
+        // else{
+        //   window.alert("It's Working Day")
+        // }
+//---------------------------------------------------
+        //question no 5
+
+        // var d = new Date();
+        
+        // if(d<=15){
+        //     window.alert('First fifteen days of the month')
+
+        // }
+        // else if (d>=16){
+        //   window.alert('Last fifteen days of the month')
+        // }
+//-------------------------------------------------------
+        //question no 6
+
+      //   var a,b,c,d,e,f,g;
+      //   a=new Date();
+      //   b=new Date();
+      //   c=new Date();
+      //   d=a.getHours();
+      //   e=b.getSeconds();
+      //   f=c.getMinutes();
+      //  g=d,e,f;
+      //  if(g<=12){
+      //      window.alert("Its AM")
+      //  }
+      //  else {
+      //    window.alert("Its PM")
+      //  }
+//-----------------------------------------------------------------------
+            //question no 7
+            //       var d = new Date();
+            // var n = d.getUTCSeconds();
+        
+            //   alert(n);
+        
+                // var doomsday = new Date("June 30, 2035");
+                // alert( doomsday);
+                // var msToday = today.getTime();
+                // var msDoomsday = doomsday.getTime();
+
+//--------------------------------------------------------------
+                //question no8
+                //  var a,b,c,d;
+                // a=new Date("june,18,2015");
+                // b=new Date();
+                // c=b/a;
+                // d=c.round();
+                // alert(d)
+//---------------------------------------------------------------------
+                //question no 10
+                // var a,b,c,d;
+                // a=new Date("dec,05,2020")
+                // b=new Date("dec ,05,2020")
+                // // b=new Date();
+
+            
+                // c=b.setUTCSeconds(b.getSeconds());
+
+                
+                // alert('reference date'+a+'\n'+c +' seconds had passed since begining of 2020');
+//------------------------------------------------------------------------------------------
+                //question  no 11
+                // var a,b,c,d;
+                // a=new Date();
+                // b=new Date();
+                // c=b.setUTCHours(b.getUTCHours() - 1);
+                
+                
+                
+                // alert('Current date: '+a+'\n 1 hour ago ,it was '+b);
+
+                  
+   //---------------------------------------------------------------             
+                // // question no 12
+                //   var a,b,c,d;
+                //   a=new Date();
+                //   b=new Date((100));
+                //   alert(b);
+//----------------------------------------------------
+                  //question no 13
+
+                  // var a,b,c,d;
+                  // a=prompt("Enter your age is" );
+                  
+                  // b=new Date();
+                  // c=a-b;
+                  // alert(b);
+
+//---------------------------------------------------------------------------
+          //chapter 35-38
+  //------------------------------------------
+          //question no 1
+   //-----------------------------------------------       
+        // var a = new Date();
+        // document.write(a);
+//---------------------------------------------
+          //Q#2
+          
+      // function fullName() {
+      //      var firstName = "Arham";
+      //      var lastName = "khan";
+      //      alert("Full Name is :"+ " " + firstName + " "+ lastName);
+      // }
+      // fullName();
+//----------------------------------------------------
+      //question no 3
+        // function Sum(a,b){
+        //     var z = a+b
+        //     return z;
+        // }
+        // var c = Sum(4,8);
+        // alert(c);
+//-----------------------------------------------------
+
+    //question no 4
+      // function calc(num1,opr,num2){
+      //     if(opr === "+"){
+      //         return num1 + num2
+      //     }
+      //     else if(opr === "-"){
+      //         return num1 - num2
+      //     }
+      //     else if(opr === "*"){
+      //         return num1 * num2
+      //     }
+      //     else{
+      //         return "Incorrect Operator!"
+      //     }
+      // }
+      // var result = calc(5,"+",5);
+      // var result1 = calc(4,"*",6);
+      // var result2 = calc(4,"$",6);
+      // document.write(result +" ")
+      // document.write(result1 +"  ")
+      // document.write(result2)
+//-----------------------------------------------------------------------
+          //question  no5
+          // function squa(a){
+          //    var b = a*a;
+          //    document.write(b)
+          // }
+            // squa(4)
+
+//-----------------------------------------------------------
+      ///question no 6
+      // function fact(n){
+      //     var b = n * (n-1) *(n-2);
+      //     document.write(b) 
+      // }
+      // fact(4)
+
